a065a310ade7857fdb74a28e926e208cdbdfe109
daniromero22 <romedani02@gmail.com>
2023-09-26 09:15:29 +0200

    primera subida

--- src/marquesDeComaresEntornos/holaMundo.java
+++ src/marquesDeComaresEntornos/holaMundo.java
@@ -3,7 +3,7 @@ package marquesDeComaresEntornos;
 public class holaMundo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hola Mundo");
 
 	}
 
